00c27956486cd45b141fa9d40ae1310ae99dd31f
Jeremy Rossi <jeremy@jeremyrossi.com>
2013-09-16 10:02:34 -0400

    Merging

--- contrib/ossec-testing/runtests.py
+++ contrib/ossec-testing/runtests.py
@@ -1,21 +1,25 @@
 import ConfigParser
-import subprocess 
-import os 
+import subprocess
+import os
 import sys
-import os.path 
+import os.path
 
 class OssecTester(object):
     def __init__(self):
-        self._debug = False 
-        self._quiet = False 
+        self._debug = False
+        self._quiet = False
         self._ossec_conf = "/var/ossec/etc/ossec.conf"
         self._base_dir = "/var/ossec/"
         self._ossec_path = "/var/ossec/bin/"
+<<<<<<< local
+        self._test_path = "./tests"
+=======
         self._ossec_path = "/home/jrossi/src/ossec-hids-main/src/analysisd/"
         self._test_path = "./tests" 
+>>>>>>> other
 
     def buildCmd(self, rule, alert, decoder):
-        cmd = ['%s/ossec-logtest'%(self._ossec_path),] 
+        cmd = ['%s/ossec-logtest'%(self._ossec_path),]
         if self._ossec_conf: cmd += ["-c",self._ossec_conf]
         if self._base_dir: cmd += ["-D", self._base_dir]
         cmd += ['-U', "%s:%s:%s"%(rule,alert,decoder)]
@@ -30,18 +34,18 @@ class OssecTester(object):
                 shell=False)
         std_out = p.communicate(log)[0]
         if (p.returncode != 0 and not negate) or (p.returncode == 0 and negate):
-            print "" 
+            print ""
             print "-" * 60
-            print "Failed: Exit code = %s"%(p.returncode) 
-            print "        Alert     = %s"%(alert) 
+            print "Failed: Exit code = %s"%(p.returncode)
+            print "        Alert     = %s"%(alert)
             print "        Rule      = %s"%(rule)
             print "        Decoder   = %s"%(decoder)
             print "        Section   = %s"%(section)
             print "        line name = %s"%(name)
-            print " " 
-            print std_out 
+            print " "
+            print std_out
         elif self._debug:
-            print "Exit code= %s"%(p.returncode) 
+            print "Exit code= %s"%(p.returncode)
             print std_out
         else:
             sys.stdout.write(".")
@@ -60,20 +64,20 @@ class OssecTester(object):
                     decoder = tGroup.get(t, "decoder")
                     for (name, value) in tGroup.items(t):
                         if name.startswith("log "):
-                            if self._debug: 
+                            if self._debug:
                                 print "-"* 60
                             if name.endswith("pass"):
-                                neg = False 
+                                neg = False
                             elif name.endswith("fail"):
                                 neg = True
                             else:
-                                neg = False 
+                                neg = False
                             self.runTest(value, rule, alert, decoder, t, name, negate=neg)
                 print ""
 
 if __name__ == "__main__":
     OT = OssecTester()
-    OT.run() 
+    OT.run()
 
 
 
